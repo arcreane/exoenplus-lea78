d9680480ec03fb56a6135f80754e36bd701fbc6d
lea78 <lea.hamzi@edu.itescia.fr>
2020-11-02 19:26:37 +0100

    création menu ds main pour choix de jeu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Palindrome.enterWord();
+        generalMenu();
+
+    }
+    public static void generalMenu(){
+        System.out.println("Choose your game :");
+        System.out.println("0 : Guess a number");
+        System.out.println("1 : Palindrome");
+        System.out.println("3 : quit");
+
+        try {
+            int userInput;
+
+            userInput= scan.nextInt();
+            switch (userInput){
+                case 0 : GuessANumber.displayMenu();
+                case 1 : Palindrome.enterWord();
+                case 3 : break;
+                default:
+                    System.out.println("Commande inconnue");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("format inconnu");
+        }
     }
 }
--- src/com/company/Palindrome.java
+++ src/com/company/Palindrome.java
@@ -25,12 +25,13 @@ public class Palindrome {
         int forward = 0;
         int backword = lenghtText - 1;
 
-        // char = caractère a un emplacement mémoire
-        // charAt = selectionne un charactère d'une chaine de string
-        /*Ici créer une boucle qui tourne tant qu'il reste un caractère a comparer
-        * créer une variable forwardChar qui avance d'un caractère après un caractère depuis debut chaîne
-        * créer une variable backwardChar qui recule a partir du dernier caractère de la chaine de string
-        * if : si les deux caractères de la chaîne sont différent => false, ce n'est pas un palindrome*/
+        /* char = caractère a un emplacement mémoire
+         *charAt = selectionne un charactère d'une chaine de string
+         *Ici créer une boucle qui tourne tant qu'il reste un caractère a comparer
+         * créer une variable forwardChar qui avance d'un caractère après un caractère depuis debut chaîne
+         * créer une variable backwardChar qui recule a partir du dernier caractère de la chaine de string
+         * if : si les deux caractères de la chaîne sont différent => false, ce n'est pas un palindrome
+        */
         while (backword > forward){
             char forwardChar = clean.charAt(forward++);
             char backwardChar = clean.charAt(backword--);
